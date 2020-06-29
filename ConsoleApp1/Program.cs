b12dd158542f2c0237ed251cb936e8a1a78cadaa
Asif Rehman <asif.pro@gmail.com>
2020-06-30 03:51:49 +0500

    v2.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
-using System.Speech;
 using System.Threading;
 using System.Speech.Recognition;
-using System.Runtime.CompilerServices;
-using System.Speech.Synthesis;
 
 namespace ConsoleApp1
 {
@@ -26,26 +21,66 @@ namespace ConsoleApp1
         {
             var tcpListener = TcpListener.Create(1234);
             tcpListener.Start();
-            Console.WriteLine("[Server] Started to listen @port=1234");
+            Console.WriteLine($"[Server] Started to listen @port=1234 : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
             while (true)
             {
-                Console.WriteLine("[Server] waiting for the client");
-                var tcpClient = await tcpListener.AcceptTcpClientAsync();
-                Console.WriteLine("[Server] Client has connected");
-                using (var networkStream = tcpClient.GetStream())
+                try
                 {
-                    var buffer = new byte[1024];
-                    Console.WriteLine($"[Server] Reading from client : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
-                    var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
-                    var request = Encoding.UTF8.GetString(buffer, 0, byteCount);
-                    Console.WriteLine($"[Server] Client wrote {request} : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
-                    string result = await ReadAudioFromFileAsync(request);// await ReadAudioFromFileAsync(request);
-                    string ServerResponseString = result;
-                    byte[] ServerResponseBytes = Encoding.UTF8.GetBytes(ServerResponseString);
+                    Console.WriteLine($"[Server] waiting for the client : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
+                    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+                    Console.WriteLine($"[Server] Client has connected : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
+                    using (var networkStream = tcpClient.GetStream())
+                    {
+                        var buffer = new byte[1024];
+                        Console.WriteLine($"[Server] Reading from client : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
+                        var byteCount = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                        var request = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                        Console.WriteLine($"[Server] Client request to read file: {request} : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}");
+//                        string result = await ReadAudioFromFileAsync(request);  // takes 2 minutes to process 100 request in debug mode.
+                        string result = ReadAudioFromFileAsync2(request);     // takes 1 minute 8 seconds to process 100 requests in debug mode.
+                        string ServerResponseString = result;
+                        byte[] ServerResponseBytes = Encoding.UTF8.GetBytes(ServerResponseString);
 
-                    await networkStream.WriteAsync(ServerResponseBytes, 0, ServerResponseBytes.Length);
-                    Console.WriteLine($"[Server] Response : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}\n{result}");
+                        await networkStream.WriteAsync(ServerResponseBytes, 0, ServerResponseBytes.Length);
+                        Console.WriteLine($"[Server] Response against request:{request} : ThreadId={Thread.CurrentThread.ManagedThreadId.ToString()}\n{result}");
+                    }
+
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message.ToString());
+                }
+            }
+
+            string ReadAudioFromFileAsync2(string request)
+            {
+                string result = "";
+                bool complete = false;
+
+                using (SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine())
+                {
+                    Grammar dictation = new DictationGrammar();
+                    dictation.Name = "Dictation Grammar";
+                    recognizer.LoadGrammarAsync(dictation);
+                    recognizer.SetInputToWaveFile(request);
+                    recognizer.SpeechRecognized += (sender, e) =>
+                    {
+                        result = e.Result.Text;
+                        complete = true;
+                    };
+                    recognizer.RecognizeCompleted += (sender, e) =>
+                    {
+                        complete = true;
+                    };
+                    recognizer.RecognizeAsync();
+                    while (!complete)
+                    {
+
+                    }
+                    return result;
+
+                }
+
             }
         }
 
@@ -74,6 +109,6 @@ namespace ConsoleApp1
                 recognizer.Recognize();
                 return tcs.Task;
             }
-       }
+        }
     }
 }
